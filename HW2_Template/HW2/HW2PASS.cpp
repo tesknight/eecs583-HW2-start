7c79cc5c70149728b6763a0cfa1e18ad639b3a76
tesknight <liyuqi@umich.edu>
2023-02-04 13:38:51 -0500

    start

---
+++ HW2_Template/HW2/HW2PASS.cpp
@@ -0,0 +1,110 @@
+//===-- Frequent Path Loop Invariant Code Motion Pass --------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===---------------------------------------------------------------------===//
+//
+// EECS583 W23 - This pass can be used as a template for your FPLICM homework
+//               assignment.
+//               The passes get registered as "fplicm-correctness" and
+//               "fplicm-performance".
+//
+//
+////===-------------------------------------------------------------------===//
+#include "llvm/Analysis/BlockFrequencyInfo.h"
+#include "llvm/Analysis/BranchProbabilityInfo.h"
+#include "llvm/Analysis/LoopInfo.h"
+#include "llvm/Analysis/LoopIterator.h"
+#include "llvm/Analysis/LoopPass.h"
+#include "llvm/IR/CFG.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Transforms/Scalar/LoopPassManager.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
+#include "llvm/Transforms/Utils/LoopUtils.h"
+
+/* *******Implementation Starts Here******* */
+// You can include more Header files here
+/* *******Implementation Ends Here******* */
+
+using namespace llvm;
+
+namespace Correctness{
+struct FPLICMPass : public LoopPass {
+  static char ID;
+  FPLICMPass() : LoopPass(ID) {}
+
+  bool runOnLoop(Loop *L, LPPassManager &LPM) override {
+    bool Changed = false;
+
+    /* *******Implementation Starts Here******* */
+
+
+    /* *******Implementation Ends Here******* */
+
+    return Changed;
+  }
+
+  void getAnalysisUsage(AnalysisUsage &AU) const override {
+    AU.addRequired<BranchProbabilityInfoWrapperPass>();
+    AU.addRequired<BlockFrequencyInfoWrapperPass>();
+    AU.addRequired<LoopInfoWrapperPass>();
+    // You can add more Analysis Passes, if needed.
+  }
+
+private:
+  /// Little predicate that returns true if the specified basic block is in
+  /// a subloop of the current one, not the current one itself.
+  bool inSubLoop(BasicBlock *BB, Loop *CurLoop, LoopInfo *LI) {
+    assert(CurLoop->contains(BB) && "Only valid if BB is IN the loop");
+    return LI->getLoopFor(BB) != CurLoop;
+  }
+
+};
+} // end of namespace Correctness
+
+char Correctness::FPLICMPass::ID = 0;
+static RegisterPass<Correctness::FPLICMPass> X("fplicm-correctness", "Frequent Loop Invariant Code Motion for correctness test", false, false);
+
+
+namespace Performance{
+struct FPLICMPass : public LoopPass {
+  static char ID;
+  FPLICMPass() : LoopPass(ID) {}
+
+  bool runOnLoop(Loop *L, LPPassManager &LPM) override {
+    bool Changed = false;
+
+    /* *******Implementation Starts Here******* */
+
+
+    /* *******Implementation Ends Here******* */
+
+    return Changed;
+  }
+
+  void getAnalysisUsage(AnalysisUsage &AU) const override {
+    AU.addRequired<BranchProbabilityInfoWrapperPass>();
+    AU.addRequired<BlockFrequencyInfoWrapperPass>();
+    AU.addRequired<LoopInfoWrapperPass>();
+    // You can add more Analysis Passes, if needed.
+  }
+
+private:
+  /// Little predicate that returns true if the specified basic block is in
+  /// a subloop of the current one, not the current one itself.
+  bool inSubLoop(BasicBlock *BB, Loop *CurLoop, LoopInfo *LI) {
+    assert(CurLoop->contains(BB) && "Only valid if BB is IN the loop");
+    return LI->getLoopFor(BB) != CurLoop;
+  }
+
+};
+} // end of namespace Performance
+
+char Performance::FPLICMPass::ID = 0;
+static RegisterPass<Performance::FPLICMPass> Y("fplicm-performance", "Frequent Loop Invariant Code Motion for performance test", false, false);
